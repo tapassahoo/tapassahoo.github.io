25769a4e01036993b0ced7de685f51b9798421b9
“Tapas <tapascuchem@gmail.com>
2025-04-13 01:54:21 +0530

    Darwin: Updated on 13-04-2025

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
   document.getElementById("nav-links").classList.toggle("show");
 }
 
+function toggleMenu() {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+}
+
